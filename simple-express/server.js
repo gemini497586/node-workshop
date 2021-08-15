510fc513f9c3404d049011b0541446dfde202a07
Chien-chen Wu <gemini497586@gmail.com>
2021-08-15 11:36:55 +0800

    add two middleware

--- simple-express/server.js
+++ simple-express/server.js
@@ -2,6 +2,21 @@ const express = require("express");
 
 let app = express();
 
+app.use((req, res, next) => {
+  console.log("點餐～～吃早餐了");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("快樂早餐時光");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("午餐吃什麼？");
+  next();
+});
+
 app.get("/", function (request, response, next) {
   response.send("hello");
 });
