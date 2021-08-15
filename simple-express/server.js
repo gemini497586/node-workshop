3134090e6eefbdb658007ca3f7026528f2738cf5
Chien-chen Wu <gemini497586@gmail.com>
2021-08-15 10:47:16 +0800

    add /about~~~

--- simple-express/server.js
+++ simple-express/server.js
@@ -6,6 +6,10 @@ app.get("/", function (request, response, next) {
   response.send("hello");
 });
 
+app.get("/about", (request, response, next) => {
+  response.send("關於我們~~~~~~~~~~~~");
+});
+
 app.listen(3000, function () {
   console.log("我們的web server 啟動了~~~");
 });
